e2020b7c9d04d0dc70203235f599affb448bbd1d
Diebarix <diegoabr0704@gmail.com>
2023-06-07 23:41:20 -0600

    basic form

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,5 +1,9 @@
 import { Enlace } from "Enlace";
+import { ReadState } from "components/ReadState";
 import { Link } from "react-router-dom";
+import { GearApi } from "@gear-js/api";
+import { useState } from "react";
+import { Button } from "@gear-js/ui";
 import faker from "faker";
 import {
 	Chart as ChartJS,
@@ -111,9 +115,37 @@ export const data2 = {
 	],
 };
 function Home() {
+	const [chainData, setChain] = useState<string>();
+	const [nodeNameData, setNodeName] = useState<string>();
+	const [nodeVersionData, setNodeVersion] = useState<string>();
+
+	const nodeInformation = async () => {
+		const gearApi = await GearApi.create({
+			providerAddress: "wss://rpc-node.gear-tech.io",
+		});
+
+		const [chain, nodeName, nodeVersion] = await Promise.all([
+			gearApi.chain(),
+			gearApi.nodeName(),
+			gearApi.nodeVersion(),
+		]);
+
+		setChain(chain);
+		setNodeName(nodeName);
+		setNodeVersion(nodeVersion);
+	};
+
 	return (
 		<>
 			<Enlace title="Water Statistics" />
+			<div className="card">
+				<h3>Node Data</h3>
+				<p>Red: {chainData}</p>
+				<p>Nodo: {nodeNameData}</p>
+				<p>Version del nodo:{nodeVersionData}</p>
+				<Button text="Get Node Information" onClick={nodeInformation} />
+				<ReadState />
+			</div>
 			<div className={styles.fondo}>
 				<div className={styles.container_btn_register}>
 					<img className={styles.arrow} src={arrow} alt="" />
