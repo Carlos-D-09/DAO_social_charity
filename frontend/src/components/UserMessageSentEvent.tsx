a3eedbd3eaf27cda111d6f358ece1b26dbba04f3
Guadalupe Gutierrez <jose.gutierrez1319@alumnos.udg.mx>
2023-06-08 11:06:32 -0600

    Updated progamId

--- frontend/src/components/UserMessageSentEvent.tsx
+++ frontend/src/components/UserMessageSentEvent.tsx
@@ -13,7 +13,7 @@ function UserMessageSent() {
     const codeId =
       "0x109ffa89f6886b0ff2a8dad5c62ef45a838c78e063f998f85e0cd069b20f49dd";
     const programId =
-      "0x4d47b38aa8a9b1f896283445328a43802d95f2fb413017aa90d1f668809b24d1";
+      "0x3536201e1a84aa283ce1d4a72f0aa0c643f27d39b264a883f05480a5f82d8ce8";
 
     const somePayload = "0x676574"; // get "0x676574" inc: '0x696E63'
     const meta: any = getProgramMetadata(programId);
