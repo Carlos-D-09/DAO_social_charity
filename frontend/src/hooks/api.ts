6f64ea2e23e5a9ba6c105ede1ae0e11de32ddde4
Diebarix <diegoabr0704@gmail.com>
2023-06-08 00:13:40 -0600

    other implements

--- frontend/src/hooks/api.ts
+++ frontend/src/hooks/api.ts
@@ -1,4 +1,4 @@
-import { useSendMessage } from "@gear-js/react-hooks";
+/* import { useSendMessage } from "@gear-js/react-hooks";
 import metaTxt from "../assets/meta/meta.txt";
 import { ADDRESS } from "consts";
 import { useMetadata } from "./useMetadata";
@@ -13,3 +13,27 @@ function useSendNFTMessage() {
 }
 
 export { useNFTMetadata, useSendNFTMessage };
+ */
+import stateMetaWasm from "assets/wasm/nft_state.meta.wasm";
+import { useMetadata, useWasmMetadata } from "./useMetadata";
+import metaTxt from "assets/meta/meta.txt";
+import {
+	useAccount,
+	useReadWasmState,
+	useSendMessage,
+} from "@gear-js/react-hooks";
+import { ADDRESS } from "consts";
+
+function useNFTMetadata() {
+	return useMetadata(metaTxt);
+}
+
+function useNFTState<T>(functionName: string, payload?: any) {
+	const { buffer } = useWasmMetadata(stateMetaWasm);
+	return useReadWasmState<T>(
+		ADDRESS.CONTRACT_ADDRESS,
+		buffer,
+		functionName,
+		payload
+	);
+}
